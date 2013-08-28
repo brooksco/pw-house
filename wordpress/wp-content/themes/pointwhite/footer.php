6e519067e7bf0d8c814721c74cdf063c470277d0
Colin Brooks <colinbooks@gmail.com>
2013-08-27 17:04:15 -0700

    so many changes

--- wordpress/wp-content/themes/pointwhite/footer.php
+++ wordpress/wp-content/themes/pointwhite/footer.php
@@ -3,13 +3,13 @@
 	<hr class="mid-line">
 
 	<p>
-		Some address, Bainbridge, WA Some zipcode, some number
+		Bainbridge Island, Washington
 	</p>
 	<p>
-		Crafted with loving care by <a href="http://colinbrooks.us">colinbrooks.us</a>
+		Crafted with care by <a href="http://colinbrooks.us">colinbrooks.us</a>
 	</p>
 
-	<a id="facebook-link-footer" href="#"><img src="/pw-house/wordpress/wp-content/themes/pointwhite/img/facebookblue.png" id="facebook-footer" /></a>
+	<a id="facebook-link-footer" href="#"><img src="/wp-content/themes/pointwhite/img/facebookblue.png" id="facebook-footer" /></a>
 
 
 </footer>
